7efdefbace1450d627358a73852e73e20e144413
Lucas Miranda <lucasmiranda.da.silva8@gmail.com>
2024-08-03 20:54:46 -0300

    Inicio

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,10 @@
+window.alert('Boas vindas ao jogo do número secreto!');
+
+// Variaveis
+let numeroSecreto = 5;
+let chute = window.prompt('Escolha um número entre 1 e 10');
+
+// Condicões
+if (chute == numeroSecreto) {//se
+  console.log('Isso ai! Você descobriu o número secreto (5)');
+} 
